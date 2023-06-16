3df2f58bb7a473d0815970925933819a857da981
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-06-16 19:06:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-function debounce(callback, delay, immediate = false) {
+function debounce(func, delay, immediate = false) {
+  let timerId;
   
+  return function() {
+    const context = this;
+    const args = arguments;
+    
+    clearTimeout(timerId);
+    
+    timerId = setTimeout(function() {
+      func.apply(context, args);
+    }, delay);
+  };let timerId;
+  
+  return function() {
+    const context = this;
+    const args = arguments;
+    
+    clearTimeout(timerId);
+    
+    timerId = setTimeout(function() {
+      func.apply(context, args);
+    }, delay);
+  };
   }
   
   module.exports = debounce;
